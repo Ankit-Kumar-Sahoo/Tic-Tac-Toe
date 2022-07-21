3955e750f6f1a6df1237f9021e000d1b98aaa25d
Ankit Kumar Sahoo <ankitsahoo0703@gmail.com>
2022-07-22 01:47:35 +0530

    final

--- app/src/main/java/com/ankitkumarsahoo/tictactoe/MainActivity.java
+++ app/src/main/java/com/ankitkumarsahoo/tictactoe/MainActivity.java
@@ -20,7 +20,8 @@ public class MainActivity extends AppCompatActivity {
 
     boolean gameIsActive = true;
 
-    int [][] winningPositions = {{0, 1, 2},
+    int [][] winningPositions =
+            {{0, 1, 2},
             {3, 4, 5},
             {6, 7, 8},
             {0, 3, 6},
